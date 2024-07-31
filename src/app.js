0d17abc721a8e00a3e835d32b2514601dc0eefc8
TiToCirkus <73978802+TiToCirkus@users.noreply.github.com>
2024-07-31 23:30:10 +0000

    modificacion de archivos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogges", "racom"];
+let ext = ["com", "es"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function comparar(extension, sujeto) {
+  let extracto = "";
+  for (let i = 3; i > 0; i--) {
+    if (sujeto[sujeto.length - i] == extension[extension.length - i]) {
+      extracto = extracto + sujeto[sujeto.length - i];
+      if (extracto == extension) {
+        break;
+      }
+    }
+  }
+  return extracto;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function modificar(extension, sujeto) {
+  let extracto = "";
+  let modificacion = "";
+  for (let i = 3; i > 0; i--) {
+    if (sujeto[sujeto.length - i] == extension[extension.length - i]) {
+      extracto = extracto + sujeto[sujeto.length - i];
+      if (extracto == extension) {
+        modificacion = sujeto.slice(0, sujeto.length - extracto.length);
+        break;
+      }
+    }
+  }
+  return modificacion;
+}
+
+pronoun.forEach((articulo) => {
+  adj.forEach((adjetivo) => {
+    noun.forEach((sujeto) => {
+      ext.forEach((extension) => {
+        if (comparar(extension, sujeto) == extension) {
+          console.log(
+            articulo + adjetivo + modificar(extension, sujeto) + "." + extension
+          );
+        } else {
+          console.log(articulo + adjetivo + sujeto + "." + extension);
+        }
+      });
+    });
+  });
+});
